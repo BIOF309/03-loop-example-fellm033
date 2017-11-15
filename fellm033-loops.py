ddeaeb09afe2cf16a0363de7a4265628e56624ef
fellm033 <fellm033@gmail.com>
2017-11-14 20:16:53 -0500

    loops

---
+++ fellm033-loops.py
@@ -0,0 +1,4 @@
+family = ['A', 'D', 'G', 'C']
+notes = ['A, E, D', 'D, G, Em, A', 'G, Am, C, D, Em', 'C, Dm, Em, F, G, Am']
+for i in range(4):
+    print('The notes in ', family[i], ' are ', notes[i] )
